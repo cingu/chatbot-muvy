f1b11db65a2646fad4c441029ae4d47fb059eb0a
cingu <32195293+cingu@users.noreply.github.com>
2017-11-16 16:46:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function greetingText1(event) {
 
  request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    //EAAVYnwdT98YBAM9nYXkELlPlM1tAszeZB60eRBZAU1Q7hCSV1UHjo52NUvLIrQ9Iba9OlxPIuNGqZAEJJvxtrIM05ULQzNotFZAG26hEPhe8oGy38HEoVxasBOofsQTCFJMW5SnWehQ08YWMfsNjDTt5akBytmV1ZCZAj49J1WgwZDZD
+    
     qs: {access_token: process.env.FB_ACCESS_TOKEN}, /********* 
     method: 'POST',
     json: {
